0cfd82c27c8ee01957e341fb68e860ea48aba7f1
omkarsp121 <omkarpomendkar100@gmail.com>
2021-09-18 17:02:17 +0530

    Recursion Updated

--- JAVAPrograms/DataStructure/src/Recursion/Recurssion.java
+++ JAVAPrograms/DataStructure/src/Recursion/Recurssion.java
@@ -1,13 +1,25 @@
 package Recursion;
-
+// RECURSION - Function Calling itsSelf
+// Why - It helps us to sole bigger & Complex Problem in Simple Way
+//
 public class Recurssion {
     public static void main(String[] args) {
-        System.out.println("Learning Recursion");
-        message(5);
+        // write a function thata takes in a number and prints it
+        // print first 5 numbers : 1 2 3 4 5
+       // System.out.println("Learning Recursion");
+        print(0);
     }
 
-     static void message(int n) {
+     static void print(int n) {
+
+        if(n == 5) {
+            System.out.println(5);
+            return;
+        }
         System.out.println(n);
-        message(n+1);
+
+        // recursive call
+         // if u are calling a function again  & again
+        print(n+1);
     }
 }
